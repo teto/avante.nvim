22f408e5c03030be174916804b4a544ca47b73df
teto <886074+teto@users.noreply.github.com>
2026-09-23 18:02:32 +0200

    feat!: change default header to show current model
    I feel it helps the user knows where he is at, checking his changes were
    applied etc. I know I feel more confident seeing it. Codex shows model
    by default (claude not I believe).
    And we dont lose space, the sidebar should be wide enough to display the
    model. If it's not, the user is really using a thin window (let me know
    otherwise).
    
    You can restore the old behaviour by setting
    vim.g.avante["window"]["behavior"]["include_model"] = false

--- lua/avante/config.lua
+++ lua/avante/config.lua
@@ -814,7 +814,7 @@ M._defaults = {
       enabled = true, -- true, false to enable/disable the header
       align = "center", -- left, center, right for title
       rounded = true,
-      include_model = false,
+      include_model = true,
     },
     spinner = {
       editing = {
